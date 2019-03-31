ee94ec83e03363c7f0a6777b2516458edd97507e
Karl Stenerud <kstenerud@gmail.com>
2019-03-31 16:42:47 +0000

    Stop early if a refresh step fails

--- refresh.sh
+++ refresh.sh
@@ -5,6 +5,6 @@ set -eu
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 
 cd "$SCRIPT_DIR"
-git pull
-git submodule update --init --recursive
+git pull &&
+git submodule update --init --recursive &&
 docker restart ts-hugo
