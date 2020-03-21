ef0564d1238459bb64acc89fbf5183032bec6607
Karl Stenerud <kstenerud@gmail.com>
2020-03-21 11:50:01 +0100

    Don't specify arch when installing docker

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ apt install -y \
     software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" && \
 apt update && \
